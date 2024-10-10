af4ab4a032d2779c3698712e8b1f76fe1482d978
JKnightStrke <jnitschke@adam.com.au>
2024-10-10 21:41:27 +1030

    make layout

--- scripts.js
+++ scripts.js
@@ -1,18 +1,22 @@
 let database={}
+let focus=0
 function uploadFile(file){
 	const reader=new FileReader()
 	reader.onload=e=>{
 		database=JSON.parse(e.target.result)
-		renderPeople()}
+		renderPeople()
+	}
 	reader.readAsText(event.target.files[0])
 }
 function downloadFile(){
 	const file=new Blob(
 		[JSON.stringify(database)],
-		{type:`application/json`})
+		{type:`application/json`}
+	)
 	const link=Object.assign(document.createElement(`a`),{
 		href:URL.createObjectURL(file),
-		download:`Family Tree.json`})
+		download:`Family Tree.json`}
+	)
 	link.click()
 	URL.revokeObjectURL(link.href)
 }
@@ -31,18 +35,20 @@ function newPerson(){
 }
 function renderPeople(){
 	let contentQueue=``
-	for(let i1=0;i1<Object.keys(database).length;i1++){
-		let pointer=database[i1]
-		let name=`${pointer.fName} <span class="person-name-middle">${pointer.mName}</span> ${pointer.lName}`
-		contentQueue+=`
-			<div class="person">
-				<div class="person-name">
-					${name}
-				</div>
-				<div class="person-details">
-					${pointer.dob}
-				</div>
-			</div>`
-	}
-	document.getElementById(`content`).innerHTML=contentQueue
+	// for(let i1=0;i1<Object.keys(database).length;i1++){
+	let pointer=database[focus]
+	let name=`${pointer.fName} <span class="person-name-middle">${pointer.mName}</span> ${pointer.lName}`
+	contentQueue+=`
+		<div class="person">
+			<div class="person-name">
+				${name}
+			</div>
+			<div class="person-details">
+				${pointer.dob}
+			</div>
+		</div>`
+	document.getElementById(`focus-details`).innerHTML=contentQueue
+
+	// }
+	// document.getElementById(`content`).innerHTML=contentQueue
 }
